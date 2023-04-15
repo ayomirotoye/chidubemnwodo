4ceb7983973ef2be6aafb79f480ee2fc28f7d3a2
Dubem <nwodochidubem1@gmail.com>
2023-04-14 21:09:22 -0400

    feat: Work on ClockIn and Employee card

--- src/components/EmployeeCard.jsx
+++ src/components/EmployeeCard.jsx
@@ -1,20 +1,31 @@
-import { Avatar, Card, CardHeader } from '@mui/material'
-import React from 'react'
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 function EmployeeCard() {
   return (
-<Card>
-      <CardHeader
-        avatar={
-          <Avatar>
-            {/* Insert employee avatar here */}
-          </Avatar>
-        }
-        title="Employee Name"
-        subheader="employee.email@example.com"
+    <Card sx={{ maxWidth: 240, minWidth:"fit-content" }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Employee photo"
       />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Employee Name
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          employee.mail@example.com
+        </Typography>
+      </CardContent>
     </Card>
-  )
+  );
 }
 
-export default EmployeeCard
+export default EmployeeCard;
--- src/pages/ClockInClockOut.jsx
+++ src/pages/ClockInClockOut.jsx
@@ -1,32 +1,51 @@
-import { Button, Grid } from '@mui/material'
-import React from 'react'
-import EmployeeCard from '../components/EmployeeCard'
+import { Button, Grid } from "@mui/material";
+import React from "react";
+import EmployeeCard from "../components/EmployeeCard";
 
 function ClockInClockOut() {
   const handleClockIn = () => {
     // handle clock in logic
-  }
+  };
 
   const handleClockOut = () => {
     // handle clock out logic
-  }
+  };
   return (
-    <Grid px="2rem" container spacing={2} justifyContent="center" alignItems="center" minHeight="100vh">
-      <Grid item xs={12} sm={6} md={4}>
-        <EmployeeCard />
+    <Grid
+      px="2rem"
+      container
+      spacing={8}
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      direction="column"
+    >
+      <Grid item xs={12} container justifyContent="center" alignItems="center">
+        <Grid item>
+          <EmployeeCard />
+        </Grid>
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Button variant="contained" color="primary" onClick={handleClockIn}>
-          Clock In
-        </Button>
-      </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Button variant="contained" color="secondary" onClick={handleClockOut}>
-          Clock Out
-        </Button>
+      <Grid
+        item
+        xs={12}
+        container
+        justifyContent="center"
+        alignItems="center"
+        spacing="1rem"
+      >
+        <Grid item>
+          <Button variant="contained" color="info" onClick={handleClockIn}>
+            Clock In
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" color="warning" onClick={handleClockOut}>
+            Clock Out
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
-  )
+  );
 }
 
-export default ClockInClockOut
+export default ClockInClockOut;
