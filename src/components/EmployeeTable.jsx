f26ce34b2fe1cc392eff343eb81a95f6ee2708e6
Dubem <nwodochidubem1@gmail.com>
2023-04-14 23:40:08 -0400

    feat: update login and employee table

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -1,8 +1,94 @@
-import React from 'react'
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from '@mui/material';
+import React, { useState } from 'react'
 
-function EmployeeTable() {
+
+
+
+
+
+  const EmployeeTable = () => {
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const columns = [
+      { id: 'name', label: 'Name', minWidth: 170 },
+      { id: 'email', label: 'Email', minWidth: 100 },
+      { id: 'date', label: 'Date', minWidth: 100 },
+      { id: 'clockIn', label: 'Clock In', minWidth: 100 },
+      { id: 'clockOut', label: 'Clock Out', minWidth: 100 },
+    ];
+
+    function createData(name, email, date, clockIn, clockOut) {
+      return { name, email, date, clockIn, clockOut };
+    }
+
+    const rows = [
+      createData('John Doe', 'john.doe@example.com', '2022-04-15', '08:00', '16:30'),
+      createData('Jane Doe', 'jane.doe@example.com', '2022-04-15', '07:45', '16:15'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('Bob Smith', 'bob.smith@example.com', '2022-04-15', '08:10', '17:00'),
+      createData('John Doe', 'john.doe@example.com', '2022-04-15', '08:00', '16:30'),
+      createData('John Doe', 'john.doe@example.com', '2022-04-15', '08:00', '16:30'),
+    ];
+
+    const handleChangePage = (event, newPage) => {
+      setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+      setRowsPerPage(+event.target.value);
+      setPage(0);
+    };
   return (
-    <div>EmployeeTable</div>
+    <Paper sx={{width:'100%'}}>
+      <TableContainer sx={{maxHeight: 440}}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map((column) => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   )
 }
 
--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { Avatar, Box, Button, Container, Grid, Link, Typography } from '@mui/material'
+import { LockOutlined } from '@mui/icons-material'
+
 
 function LogIn() {
   return (
-    <div>LogIn</div>
+    <Container component="main" maxWidth="xs">
+
+        <Box
+          sx={{
+
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent:'center',
+            minHeight:'100vh'
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in to continue
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            {/* <Grid container>
+
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid> */}
+          </Box>
+        </Box>
+
+      </Container>
   )
 }
 
